5ed0bc035b0c3abcff9f5d4dde956dbe1603bdef
Giovanni Bassi <giggio@giggio.net>
2016-12-29 17:08:35 -0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,22 +11,22 @@ check () {
 #last commit message:
 MESSAGE=$(git log -1 --pretty=%B)
 echo Last commit message is "'"$MESSAGE"'"
-RELEASE_DIR=$HOME/transempregos-portal-release
-echo Cloning to $RELEASE_DIR
-git clone git@github.com:TransEmpregos/transempregos-portal-release.git $RELEASE_DIR
+RELEASE_DIR="$HOME/transempregos-portal-release"
+echo Cloning to "$RELEASE_DIR"
+git clone git@github.com:TransEmpregos/transempregos-portal-release.git "$RELEASE_DIR"
 check $?
 echo Deleting old files...
 cd $RELEASE_DIR
 find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 check $?
 echo Copying release files from "'"$SNAP_WORKING_DIR"'" to "'"$RELEASE_DIR"'"...
-cp -R $SNAP_WORKING_DIR/dist/. $RELEASE_DIR/
+cp -R "$SNAP_WORKING_DIR/dist/." "$RELEASE_DIR/"
 check $?
 echo Adding files to git...
 git add -A
 check $?
 echo Committing...
-git commit -m $MESSAGE
+git commit -m "$MESSAGE"
 check $?
 echo Pushing files to git...
 git push origin master
