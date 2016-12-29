701f643f0920bebad1cc21623e3b131655bd1687
Giovanni Bassi <giggio@giggio.net>
2016-12-29 17:01:40 -0200

    Logging on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ check () {
 
 #last commit message:
 MESSAGE=$(git log -1 --pretty=%B)
+echo Last commit message is "'"$MESSAGE"'"
 RELEASE_DIR=$HOME/transempregos-portal-release
 echo Cloning to $RELEASE_DIR
 git clone git@github.com:TransEmpregos/transempregos-portal-release.git $RELEASE_DIR
@@ -18,12 +19,15 @@ echo Deleting old files...
 cd $RELEASE_DIR
 find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 check $?
-echo Copying release files...
+echo Copying release files from "'"$SNAP_WORKING_DIR"'" to "'"$RELEASE_DIR"'"...
 cp -R $SNAP_WORKING_DIR/dist/. $RELEASE_DIR/
 check $?
+echo Adding files to git...
 git add -A
 check $?
+echo Committing...
 git commit -m $MESSAGE
 check $?
+echo Pushing files to git...
 git push origin master
 check $?
