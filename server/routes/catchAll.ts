7d0ec480740276bcbc769308983d7d9410908cc0
Giovanni Bassi <giggio@giggio.net>
2017-01-06 12:54:49 -0200

    Fix interpolated string

--- server/routes/catchAll.ts
+++ server/routes/catchAll.ts
@@ -6,7 +6,7 @@ const router = new Router();
 router.get('/', async ctx => {
     // log(`New call to catch all, server is down: ${upMonitor.isDown}, because ${upMonitor.reason}.`);
     if (upMonitor.isDown) {
-        log('Server is down because of ${upMonitor.reason}, not trying to go the database.');
+        log(`Server is down because of ${upMonitor.reason}, not trying to go the database.`);
         await ctx.render('index', { jobs: null, isDown: true });
         return;
     } else {
