4dc0f6b68d81338e39288cd6a62a44decd107f92
Ansumane <ansusambu@gmail.com>
2017-01-09 15:45:26 -0200

    adequaçao das coisas feitas no login

--- public/app/login/login-recruiter.component.ts
+++ public/app/login/login-recruiter.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
     moduleId: module.id,
     selector: 'trans-login-recruiter',
     templateUrl: 'login-recruiter.component.html',
-    styleUrls: ['login-recruiter.component.css']
+    styleUrls: ['login-user.component.css']
 })
 export class LoginRecruiterComponent implements OnInit {
 
