c414bdb218286f32ac52dff349bbd638db7a97cb
Giovanni Bassi <giggio@giggio.net>
2017-02-02 16:05:43 -0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,40 +7,108 @@ check () {
     exit 999
   fi
 }
-
-#last commit message:
 MESSAGE=$(git log -1 --pretty=%B)
 echo Last commit message is "'"$MESSAGE"'"
 RELEASE_DIR="$HOME/transempregos-portal-release"
-if [ -d "$RELEASE_DIR" ]; then
-  echo Deleting release dir "'"$RELEASE_DIR"'"...
-  rm -rf "$RELEASE_DIR"
-fi
-echo Cloning to "'"$RELEASE_DIR"'"...
-git clone --depth 1 git@github.com:TransEmpregos/transempregos-portal-release.git "$RELEASE_DIR"
-check $?
-echo Deleting old files...
-cd $RELEASE_DIR
-shopt -s extglob
-rm !(.git|.|..) -rf
-check $?
-echo Copying release files from "'"$SNAP_WORKING_DIR"'" to "'"$RELEASE_DIR"'"...
-mkdir dist
-cp -R "$SNAP_WORKING_DIR/dist/." "$RELEASE_DIR/dist/"
-check $?
-cp "$SNAP_WORKING_DIR/package.json" "$RELEASE_DIR/package.json"
-check $?
-echo Adding files to git...
-git add -A
-check $?
-if [[ $(git status -s) ]]; then
-  echo Committing...
-  git commit -m "#$SNAP_PIPELINE_COUNTER $MESSAGE
-Original commit: $SNAP_COMMIT
-https://github.com/TransEmpregos/transempregos-portal/commit/$SNAP_COMMIT
-On Branch: $SNAP_BRANCH"
+echo Release dir is "'"$RELEASE_DIR"'".
+
+dev() {
+  #last commit message:
+  if [ -d "$RELEASE_DIR" ]; then
+    echo Deleting release dir "'"$RELEASE_DIR"'"...
+    rm -rf "$RELEASE_DIR"
+  fi
+  echo Cloning to "'"$RELEASE_DIR"'"...
+  git clone --depth 1 git@github.com:TransEmpregos/transempregos-portal-release.git "$RELEASE_DIR"
   check $?
-  echo Pushing files to git...
-  git push origin master
+  echo Deleting old files...
+  cd $RELEASE_DIR
+  shopt -s extglob
+  #rm !(.git|.|..) -rf
   check $?
-fi
+  echo Copying release files from "'"$SNAP_WORKING_DIR"'" to "'"$RELEASE_DIR"'"...
+  mkdir dist
+  cp -R "$SNAP_WORKING_DIR/dist/." "$RELEASE_DIR/dist/"
+  check $?
+  cp "$SNAP_WORKING_DIR/package.json" "$RELEASE_DIR/package.json"
+  check $?
+  echo Adding version file...
+  echo '{ "buildNumber":'" $SNAP_PIPELINE_COUNTER, "'"commit": "'"$SNAP_COMMIT"'" }' > dist/public/version.json
+  echo Adding files to git...
+  git add -A
+  check $?
+  if [[ $(git status -s) ]]; then
+    echo Committing...
+    git commit -m "#$SNAP_PIPELINE_COUNTER $MESSAGE
+  Original commit: $SNAP_COMMIT
+  https://github.com/TransEmpregos/transempregos-portal/commit/$SNAP_COMMIT
+  On Branch: $SNAP_BRANCH"
+    check $?
+    echo Pushing files to git...
+    git push origin master
+    check $?
+  fi
+}
+
+staging() {
+  cd $RELEASE_DIR
+  check $?
+  echo Checking out staging branch...
+  git checkout staging
+  check $?
+  echo Pulling staging branch...
+  git pull --ff-only origin staging
+  check $?
+  echo Merging $SNAP_COMMIT commit - from master - into staging...
+  git merge --ff-only $SNAP_COMMIT
+  check $?
+  echo Pushing staging branch...
+  git push origin staging
+  check $?
+  echo Pushed!
+}
+
+prod() {
+  cd $RELEASE_DIR
+  check $?
+  echo Checking out staging branch...
+  git checkout prod
+  check $?
+  echo Pulling staging branch...
+  git pull --ff-only origin prod
+  check $?
+  echo Merging $SNAP_COMMIT commit - from staging - into prod...
+  git merge --ff-only $SNAP_COMMIT
+  check $?
+  echo Pushing staging branch...
+  git push origin prod
+  check $?
+  echo Pushed!
+}
+
+while getopts ":dsp" opt; do
+  case $opt in
+    d)
+      dev
+      exit 0
+      ;;
+    s)
+      staging
+      exit 0
+      ;;
+    p)
+      prod
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+echo "No option supplied." >&2
+exit 1
