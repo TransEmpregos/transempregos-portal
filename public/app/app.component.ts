90f92855ecbca3ab3e80a8b3aec9f1b14ea4f1f9
Ansumane Sambu <ansusambu@gmail.com>
2017-04-18 18:44:08 -0300

    Adicionando logout na pagina

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './user.service';
 
 @Component({
   moduleId: module.id,
@@ -9,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Transempregos';
+  private logout: any;
+  private isLoggedIn: boolean;
+  constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    this.logout = this.userService.user;
+    this.isLoggedIn = this.userService.isLoggedIn();
+    console.log(this.userService.isLoggedIn());
+  }
+
+  async logoutAsync() {
+    await this.userService.logout(); 
+    
+    this.tryToGoHome();
+
+    console.log("olá");
+  }
+
+   private tryToGoHome() {
+    this.router.navigate(['']);
+  }
 }
